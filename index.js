2a67850c26691272bce6ee536fbdfa31af5ee19b
Студент Академии <std@itstep.loc>
2022-11-16 21:00:01 +0300

    form

---
+++ index.js
@@ -0,0 +1,26 @@
+// const myModal = new HystModal({
+//     // for dynamic init() of modals
+//     // linkAttributeName: false,
+//     catchFocus: true,
+//     closeOnEsc: true,
+//     backscroll: true,
+//     beforeOpen: function(modal){
+//         console.log('Message before opening the modal');
+//         console.log(modal); //modal window object
+//     },
+//     afterClose: function(modal){
+//         console.log('Message after modal has closed');
+//         console.log(modal); //modal window object
+
+//         //If Youtube video inside Modal, close it on modal closing
+//         let videoframe = modal.openedWindow.querySelector('iframe');
+//         if(videoframe){
+//             videoframe.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*');
+//         }
+//     },
+// });
+
+const myModal = new HystModal({
+    linkAttributeName: "data-hystmodal",
+    // настройки (не обязательно), см. API
+});
